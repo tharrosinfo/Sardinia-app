bf895fece5333614c0a20d525b043d6ea5d672b7
tharrosinfo <webmaster@tharros.info>
2016-07-24 20:28:09 +0200

    ver090
    Beta version for testing

--- js/script.js
+++ js/script.js
@@ -1,7 +1,52 @@
 // script.js
 
     // create the module and name it TharrosApp
-    var TharrosApp = angular.module('TharrosApp', ['ngRoute','ui.bootstrap']);
+    var TharrosApp = angular.module('TharrosApp', ['ngRoute','ui.bootstrap','ngGeolocation','angular-confirm'], function($httpProvider) {
+		  // Use x-www-form-urlencoded Content-Type
+		  $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+
+		  /**
+		   * The workhorse; converts an object to x-www-form-urlencoded serialization.
+		   * @param {Object} obj
+		   * @return {String}
+		   */ 
+		  var param = function(obj) {
+			var query = '', name, value, fullSubName, subName, subValue, innerObj, i;
+
+			for(name in obj) {
+			  value = obj[name];
+
+			  if(value instanceof Array) {
+				for(i=0; i<value.length; ++i) {
+				  subValue = value[i];
+				  fullSubName = name + '[' + i + ']';
+				  innerObj = {};
+				  innerObj[fullSubName] = subValue;
+				  query += param(innerObj) + '&';
+				}
+			  }
+			  else if(value instanceof Object) {
+				for(subName in value) {
+				  subValue = value[subName];
+				  fullSubName = name + '[' + subName + ']';
+				  innerObj = {};
+				  innerObj[fullSubName] = subValue;
+				  query += param(innerObj) + '&';
+				}
+			  }
+			  else if(value !== undefined && value !== null)
+				query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&';
+			}
+
+			return query.length ? query.substr(0, query.length - 1) : query;
+		  };
+
+		  // Override $http service's default transformRequest
+		  $httpProvider.defaults.transformRequest = [function(data) {
+			return angular.isObject(data) && String(data) !== '[object File]' ? param(data) : data;
+		  }];
+	});
+	
 	
 	// configure our routes
     TharrosApp.config(function($routeProvider) {
@@ -14,7 +59,7 @@
             })
 			
 			// route for the sites details page
-			.when('/:id', {
+			.when('/detail/:id', {
 				templateUrl : 'pages/sites-detail.html',
                 controller  : 'detailController'
 			})
@@ -32,39 +77,40 @@
     });
 	
 	// create the controller and inject Angular's $scope
-    TharrosApp.controller('mainController', function($scope, $http, MyItems) {
-		var mydate = new Date();
-		var noCache = mydate.getTime();
-		$http.get('data/sites.json?'+noCache).success(function(data) {
-			$scope.sites = data;
-			MyItems.insert($scope.sites).then(function(message){
-				$scope.message = "Updated";
+    TharrosApp.controller('mainController', function($geolocation, $scope, $http, $filter, $confirm, MyItems) {
+		MyItems.checkstate($http,$filter,$confirm).then(function() {
+			$geolocation.getCurrentPosition({
+				timeout: 60000
+			}).then(function(position) {
+				$scope.coords = position.coords;
+				$scope.mylat = $scope.coords.latitude;
+				$scope.mylon = $scope.coords.longitude;
+				console.log("My coords "+ $scope.mylat + " , " + $scope.mylon);
+						
+				MyItems.nearest($scope.mylat,$scope.mylon).then(function(sites){
+					$scope.sites = sites;
+					$scope.message = "Overzichtslijst" ;
+					$scope.distance = function(distacos) { return (Math.acos(distacos) * 6371).toFixed(2)} ;
+				});
 			});
-		//	MyItems.all().then(function(sites){
-		//		$scope.sites = sites;
-		//		$scope.message = "Retrieved list";
-		//	});
 		});
-		
-		
-    });
+	});
 	
 	TharrosApp.controller('detailController', function($sce,$scope, MyItems, $routeParams) {
-        $scope.message = 'This is just a demo.';
-		$scope.sites = [];
+        $scope.sites = [];
 		MyItems.getById($routeParams.id).then(function(sites){
 			$scope.sites = sites;
-			$scope.infohtml = $sce.trustAsHtml(sites.info) ;//$scope.sites.info;
-			$scope.message = "Retrieved details";
+			$scope.infohtml = $sce.trustAsHtml(sites.info) ;
+			$scope.message = "Informatie";
 		});
     });
 
     TharrosApp.controller('aboutController', function($scope) {
-        $scope.message = 'Look! I am an about page.';
+        $scope.message = 'Over de app en help bij de app';
     });
 
     TharrosApp.controller('contactController', function($scope) {
-        $scope.message = 'Contact us! JK. This is just a demo.';
+        $scope.message = 'Contact informatie';
     });
 	
 	TharrosApp.controller("indexController", function($scope, $rootScope) {
@@ -109,6 +155,21 @@
 	    document.addEventListener("click", function(e) {
 	        $rootScope.$broadcast("documentClicked", e.target);
 	    });
+	});
+	
+	TharrosApp.run(function($window, $rootScope) {
+      $rootScope.online = navigator.onLine;
+      $window.addEventListener("offline", function() {
+        $rootScope.$apply(function() {
+          $rootScope.online = false;
+        });
+      }, false);
+
+      $window.addEventListener("online", function() {
+        $rootScope.$apply(function() {
+          $rootScope.online = true;
+        });
+      }, false);
 	});
 	
 	TharrosApp.run(function(db) {
